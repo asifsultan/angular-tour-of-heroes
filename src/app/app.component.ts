0b242cd40a6d2c3f16abd697fef18ff6ee7ba0ec
Asif Sultan <isultanasif@gmail.com>
2020-01-29 08:58:39 +0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MessageService } from './message.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  constructor(public messageService: MessageService) {}
+} 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,44 @@
+import  {Hero} from './hero';
+
+export const HEROES: Hero[]  =   [        //HEROES constant as an array of ten heroes 
+  { id: 1, 
+    name: 'Dr Nice',
+    detail:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam" 
+  },
+  { id: 2, 
+    name: 'Narco', 
+    detail:"Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur" 
+},
+  { id: 3, 
+    name: 'Bombasto',
+    detail: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" 
+},
+  { id: 4, 
+    name: 'Celeritas',
+   detail:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore" 
+  },
+  { id: 5, 
+    name: 'Magneta', 
+    detail:"Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur" 
+  },
+  { id: 6, 
+    name: 'RubberMan', 
+    detail:"Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur"  
+  },
+  { id: 7, 
+    name: 'Dynama', 
+    detail:" Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus omnis"  
+  },
+  { id: 8, 
+    name: 'Dr IQ', 
+   detail:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident" 
+  },
+  { id: 9, 
+    name: 'Magma', 
+    detail:"Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repella" 
+  },
+  { id: 10, 
+    name: 'Tornado', 
+    detail:"Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." 
+  }
+];
